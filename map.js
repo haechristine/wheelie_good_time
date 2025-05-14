e069344543f3c338170299c15385caf9dd970c4b
Christine Law <christine.law14013@gmail.com>
2025-05-14 12:18:06 -0700

    Update map.js

--- map.js
+++ map.js
@@ -1,6 +1,8 @@
 // Import Mapbox as an ESM module
 // import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 // Check that Mapbox GL JS is loaded
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
 console.log('Mapbox GL JS Loaded:', mapboxgl);
 
 // Set your Mapbox access token here
@@ -50,6 +52,15 @@ map.on('load', async () => {
         source: 'cambridge_route',
         paint: bikeLaneStyle,
       });
+      let jsonData;
+    try {
+        const jsonurl = 'https://dsc106.com/labs/lab07/data/bluebikes-stations.json';
 
-      console.log("Boston and Cambridge bike lanes loaded successfully.");
+        // Await JSON fetch
+        const jsonData = await d3.json(jsonurl);
+
+        console.log('Loaded JSON Data:', jsonData); // Log to verify structure
+    } catch (error) {
+        console.error('Error loading JSON:', error); // Handle errors
+    }
   });
