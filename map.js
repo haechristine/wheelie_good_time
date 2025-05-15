bf7a06590f960579b0260ef0a817f848c23a0cf3
Christine Law <christine.law14013@gmail.com>
2025-05-14 17:53:37 -0700

    finish step 3

--- map.js
+++ map.js
@@ -68,7 +68,7 @@ map.on('load', async () => {
         const jsonurl = 'https://dsc106.com/labs/lab07/data/bluebikes-stations.json';
     
         // Await JSON fetch
-        const jsonData = await d3.json(jsonurl);
+        jsonData = await d3.json(jsonurl);
     
         console.log('Loaded JSON Data:', jsonData); // Log to verify structure
       } catch (error) {
